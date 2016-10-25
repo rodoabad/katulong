68a80cd4f2373d453ba8d6f557dde2e31bb15809
rodoabad <rodoabad@gmail.com>
2016-10-24 23:17:37 -0500

    feat: add a new command for linting your style files

--- bin/katulong.js
+++ bin/katulong.js
@@ -1,21 +1,23 @@
 #! /usr/bin/env node
 const bundle = require('../lib/bundle');
 const coverage = require('../lib/coverage');
+const eslint = require('../lib/eslint');
 const init = require('../lib/init');
-const lint = require('../lib/lint');
 const preview = require('../lib/preview');
+const stylelint = require('../lib/stylelint');
 const test = require('../lib/test');
 const yargs = require('yargs');
 
 return yargs
     .usage('Usage: $0 --help')
     .strict()
-    .example('$0 lint --files ./vendor', 'Lint files in the vendor directory')
+    .example('$0 eslint --files ./vendor', 'Lint files in the vendor directory')
     .command(bundle)
     .command(coverage)
+    .command(eslint)
     .command(init)
-    .command(lint)
     .command(preview)
+    .command(stylelint)
     .command(test)
     .demand(1) // eslint-disable-line no-magic-numbers
     .help()
